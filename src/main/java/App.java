e2dc87d62cc236a36f705ddafcb8370169e96be4
ahmdmuflih <90489769+ahmdmuflih@users.noreply.github.com>
2024-06-05 09:42:18 +0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Berhasil!");
+        System.out.println("Hello, World!");
     }
 
     public static int add(int a, int b) {
